0752c7ddd1f4ce958cbc9dd07231fc7ecf4c2d9a
Jomit Jose <jomitjose0007@gmail.com>
2021-09-23 23:18:31 +0530

    Routes Modified by grouping

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,26 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 // Tags...
 Route::get('/tags', \App\Http\Controllers\TagController::class);
 
 // Offices...
-Route::get('/offices', [\App\Http\Controllers\OfficeController::class, 'index']);
-Route::get('/offices/{office}', [\App\Http\Controllers\OfficeController::class, 'show']);
-Route::post('/offices', [\App\Http\Controllers\OfficeController::class, 'create'])->middleware(['auth:sanctum', 'verified']);
-Route::put('/offices/{office}', [\App\Http\Controllers\OfficeController::class, 'update'])->middleware(['auth:sanctum', 'verified']);
-Route::delete('/offices/{office}', [\App\Http\Controllers\OfficeController::class, 'delete'])->middleware(['auth:sanctum', 'verified']);
+Route::prefix('offices/')->group(function(){
+    Route::get('/', [\App\Http\Controllers\OfficeController::class, 'index']);
+    Route::get('{office}', [\App\Http\Controllers\OfficeController::class, 'show']);
 
-// Office Photos...
-Route::post('/offices/{office}/images', [\App\Http\Controllers\OfficeImageController::class, 'store'])->middleware(['auth:sanctum', 'verified']);
-Route::delete('/offices/{office}/images/{image}', [\App\Http\Controllers\OfficeImageController::class, 'delete'])->middleware(['auth:sanctum', 'verified']);
+    Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+
+        Route::post('/', [\App\Http\Controllers\OfficeController::class, 'create']);
+        Route::put('{office}', [\App\Http\Controllers\OfficeController::class, 'update']);
+        Route::delete('{office}', [\App\Http\Controllers\OfficeController::class, 'delete']);
+
+        // Office Photos...
+        Route::post('{office}/images', [\App\Http\Controllers\OfficeImageController::class, 'store']);
+        Route::delete('{office}/images/{image}', [\App\Http\Controllers\OfficeImageController::class, 'delete']);
+    });
+
+});
+
+
