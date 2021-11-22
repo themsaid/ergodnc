1274c1346c3b459f8bcad630bbdd4ad12a6440fc
Mohamed Said <themohamedsaid@gmail.com>
2021-11-22 08:53:36 +0200

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Image;
 use App\Models\Office;
+use App\Models\Reservation;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         $user = \App\Models\User::create([
+             'name' => 'Mohamed',
+             'email' => 'mohamed@laravel.com',
+             'password' => Hash::make('asdasdasd')
+         ]);
+
+         $office1 = Office::factory()->create();
+         $office2 = Office::factory()->create();
+         $office3 = Office::factory()->create();
+
+        $office1->images()->create([
+            'path' => 'storage/1.jpg'
+        ]);
+
+        $office2->images()->create([
+            'path' => 'storage/2.jpg'
+        ]);
+
+        $office3->images()->create([
+            'path' => 'storage/3.jpg'
+        ]);
+
+        Reservation::factory()->for($user)->for($office1)->create();
+        Reservation::factory()->for($user)->for($office2)->create();
+        Reservation::factory()->for($user)->for($office3)->create();
     }
 }
